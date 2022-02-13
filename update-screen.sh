8e43eabcf05d1b89a131e36646b1c870bc95989b
Lucy Davinhart <lucy@lucymhdavies.com>
2022-02-13 18:34:18 +0000

    If we have no internet, show something specific for that

--- update-screen.sh
+++ update-screen.sh
@@ -8,35 +8,56 @@ echo script dir: ${script_dir}
 cd ${script_dir}
 echo running in... $PWD
 
-
-
 TTY=1
 FB=0
 
 current=system
 
-showFronter() {
+showImage() {
 	# http://manpages.ubuntu.com/manpages/bionic/man1/fbi.1.html
-	sudo fbi --noverbose -a -d /dev/fb${FB} -T ${TTY} images/${1}.png
+	sudo fbi --noverbose -a -d /dev/fb${FB} -T ${TTY} images/${1}.*
 }
 
-showFronter ${current}
+while : ; do
+	if ping -q -w 1 -c 1 api.lmhd.me > /dev/null ; then
+		echo We have internet
+		break
+	else
+		echo No internet
+		showImage no-internet
+		sleep 15
+	fi
+done
+
+
+
+showImage ${current}
 sleep 2
 
 while : ; do
-	new=$(curl https://api.lmhd.me/v1/fronter.json | jq -r .members[0].name)
+	if ping -q -w 1 -c 1 api.lmhd.me > /dev/null ; then
+		echo We still have internet
+	else
+		echo No internet
+		showImage no-internet
+		sleep 15
+		continue
+	fi
+
+	new=$(curl -s https://api.lmhd.me/v1/fronter.json | jq -r .members[0].name)
 
 	if [[ ${fronter} == "null" ]]; then
 		new=system
+		continue
 	fi
 
 	if [[ "${new}" != "${current}" ]]; then
 		echo "New Fronter: ${new}"
 		if [[ ${current} != "system" ]]; then
-		showFronter system
+		showImage system
 			sleep 2
 		fi
-		showFronter ${new}
+		showImage ${new}
 
 		current=${new}
 	fi
